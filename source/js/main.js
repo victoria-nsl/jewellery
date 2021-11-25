a9937435ff6a7db24d2e41536db7c90ecda642d5
victoria-nsl <vbnaish@yandex.ru>
2021-11-25 15:16:04 +0300

    Добавляет слайдер swiper и его работу в десктопной версии

--- source/js/main.js
+++ source/js/main.js
@@ -5,6 +5,8 @@ const navigationToggle = menu.querySelector('.page-header__toggle');
 const itemsAccordionFaq = document.querySelectorAll('.faq__item');
 const triggersFaq = document.querySelectorAll('.faq__button');
 
+const slider = document.querySelector('.swiper-conteiner');
+
 /*======ОТКРЫТИЕ/ЗАКРЫТИЕ МОБИЛЬНОГО МЕНЮ ============*/
 const closeMenu = () => {
   menu.classList.add('page-header--closed');
@@ -53,3 +55,41 @@ if (itemsAccordionFaq) {
     });
   });
 }
+
+/*================СЛАЙДЕР==========================================*/
+new Swiper (slider, {
+  spaceBetween: 30,
+  simulateTouch: false,
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return `<span class="${className}">${index + 1}</span>`;
+    },
+  },
+
+  breakpoints: {
+    '1024': {
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+    },
+
+    '768': {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+
+    '320': {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+  },
+});
